f32c86752e4e593573ba0d79feae3cfef065ad06
Douglas <nkubitudouglas@gmail.com>
2024-02-09 17:50:36 +0300

    feat: send email upon creating a new contact form

--- website_manager/website_manager/doctype/contact_form/contact_form.js
+++ website_manager/website_manager/doctype/contact_form/contact_form.js
@@ -1,8 +1,58 @@
 // Copyright (c) 2024, Douglas Nkubitu and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Contact Form", {
-// 	refresh(frm) {
+frappe.ui.form.on('Contact Form', {
+	after_save: function (frm) {
+		// Fetch data from the contact form document for sending email
+		frappe.call({
+			method: 'website_manager.website_manager.doctype.contact_form.contact_form.get_contact_form_data',
+			args: {
+                doc: frm.doc
+			},
+			callback: function(response) {
+				if (response && response.message) {
+					// Fetch data from contact form
+					var full_name = response.message.full_name;
+                    var email_address = response.message.email_address;
+                    var phone_number = response.message.phone_number;
+                    var company = response.message.company;
+                    var message = response.message.message;
 
-// 	},
-// });
+					frappe.call({
+						method: 'website_manager.website_manager.doctype.contact_form.contact_form.get_email_template',
+						args: {
+							template_name: 'New Inquiry',
+							doc: frm.doc,
+							full_name: full_name,
+                            email_address: email_address,
+                            phone_number: phone_number,
+                            company: company,
+                            message: message,
+						},
+						callback: function(emailResponse) {
+							if (emailResponse && emailResponse.message) {
+								// Fetch email message from response
+								var message = emailResponse.message;
+
+								// Send the email
+								frappe.call({
+									method: 'website_manager.website_manager.doctype.contact_form.contact_form.send_email',
+									args: {
+										subject: message.subject,
+										content: message.message
+									},
+									callback: function(r) {
+										if (r.exc) {
+											msgprint(r.exc);
+											return;
+										}
+									}
+								});
+							}
+						}
+					});
+				}
+			}
+		});
+	}
+})
--- website_manager/website_manager/doctype/contact_form/contact_form.py
+++ website_manager/website_manager/doctype/contact_form/contact_form.py
@@ -1,9 +1,57 @@
 # Copyright (c) 2024, Douglas Nkubitu and contributors
 # For license information, please see license.txt
 
-# import frappe
+import json
+import frappe
 from frappe.model.document import Document
 
 
 class ContactForm(Document):
 	pass
+
+@frappe.whitelist()                
+def get_contact_form_data(doc):
+    contact_form_doc = frappe.get_doc("Contact Form", doc)
+    return {
+        'full_name': contact_form_doc.full_name,
+        'email_address': contact_form_doc.email_address,
+        'phone_number': contact_form_doc.phone_number,
+        'company': contact_form_doc.company,
+        'message': contact_form_doc.message
+    }
+
+@frappe.whitelist()
+def get_email_template(template_name, doc, full_name=None, email_address=None, phone_number=None, company=None, message=None):
+    """Returns the processed HTML of an email template with the given doc and additional fields"""
+    if isinstance(doc, str):
+        doc = json.loads(doc)
+
+    email_template = frappe.get_doc("Email Template", template_name)
+    if full_name:
+        doc["full_name"] = full_name
+    if email_address:
+         doc["email_address"] = email_address
+    if phone_number:
+        doc["phone_number"] = phone_number
+    if company:
+        doc["company"] = company
+    if message:
+        doc["message"] = message
+
+    return email_template.get_formatted_email(doc)
+
+@frappe.whitelist()
+def send_email(subject, content):
+    default_email_account = frappe.get_doc("Email Account", {"default_outgoing": 1})
+
+    # Check if a default email account is configured
+    if not default_email_account:
+        error_message = "Error: No default email account configured."
+        frappe.log_error(error_message, "Default Email Account Error")
+        return error_message
+
+    frappe.sendmail(
+        recipients=default_email_account.email_id,
+        subject=subject,
+        content=content
+    )
